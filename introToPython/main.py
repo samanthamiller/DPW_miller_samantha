2eb00b77a48146b83ac8412f13142cab27f6ac20
Samantha <miller.m.samantha@gmail.com>
2014-03-04 19:56:13 -0500

    Python syntax examples (if elif else)

--- introToPython/main.py
+++ introToPython/main.py
@@ -1,2 +1,32 @@
+'''
+-----
+Samantha Miller
+March 4, 2014
+Assignment Name
+'''
+
+
+
 name = raw_input('Enter your name: ')
 print name + ', very nice to meet you!'
+
+#single lined comment
+
+#Expressions
+'''
+first = 'Samantha'
+yearBorn = 1994
+currentYear = 2014
+age = currentYear - yearBorn
+print age
+'''
+
+#conditionals
+budget = 300
+if budget > 200:
+    #stuff we wanna do it true
+    print 'You can buy a pair of Jordans'
+elif budget > 30:
+    print 'You can buy a pair of shoes from payless'
+else:
+    print 'No shoes for you'
