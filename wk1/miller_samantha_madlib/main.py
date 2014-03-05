26d7fe3958b8aeedba28af78caf6eb3425c91418
Samantha <miller.m.samantha@gmail.com>
2014-03-04 22:40:30 -0500

    Added in two dictionaries and started an if statement

--- wk1/miller_samantha_madlib/main.py
+++ wk1/miller_samantha_madlib/main.py
@@ -12,3 +12,18 @@ first_money = input('Amount of money first friend has: ')
 second_money = input('Amount of money second friend has: ')
 third_money = input('Amount of money third friend has: ')
 
+def group_amount(a1, a2, a3):
+	group_total = a1 + a2 + a3
+	return group_total
+group_amount(first_money, second_money, third_money)
+
+
+menu_dict = dict()
+menu_dict = {'large cheese pizza': 10.50, 'large cheese stuffed crust pizza': 13.50, 'large meat lovers stuffed crust pizza': 15.50}
+print menu_dict
+
+
+group_dict = dict()
+group_dict = {first_friend:first_money, second_friend:second_money, third_friend:third_money}
+
+
