4a2436d2a6c7d9a0fd4b2686489fee1d2e98eeb2
Samantha <miller.m.samantha@gmail.com>
2014-03-04 20:15:40 -0500

    Python syntax of arrays and dictionaries

--- introToPython/main.py
+++ introToPython/main.py
@@ -6,9 +6,10 @@ Assignment Name
 '''
 
 
-
+'''
 name = raw_input('Enter your name: ')
 print name + ', very nice to meet you!'
+'''
 
 #single lined comment
 
@@ -21,7 +22,8 @@ age = currentYear - yearBorn
 print age
 '''
 
-#conditionals
+#Conditionals
+'''
 budget = 300
 if budget > 200:
     #stuff we wanna do it true
@@ -30,3 +32,29 @@ elif budget > 30:
     print 'You can buy a pair of shoes from payless'
 else:
     print 'No shoes for you'
+'''
+
+#Functions
+'''
+def calcArea(h,w):
+    perimeter = 2*h + 2*w
+    a = h*w
+    return a
+
+height = 40
+width = 30
+area = calcArea(height,width)
+print 'Your area is ' + str(area)
+'''
+
+#Arrays
+'''
+students = ['Jairo', 'Austin', 'Samantha', 'Bryan', 'Lyte', 'Aurturo', 'Anthony','Mike','Ross']
+
+#Dictionary - associative arrays
+villains = dict()
+villains = {'Star Wars':'Darth Vader', 'Lion King':'Scar', 'Hannah Montana': 'Miley Cyrus'}
+print villains['Hannah Montana']
+'''
+
+#Loops
