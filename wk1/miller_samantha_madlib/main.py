a7607ab0295619caf1f13bea97fbbd09838529ff
Samantha <miller.m.samantha@gmail.com>
2014-03-04 23:44:19 -0500

    Added in if statements and some comments

--- wk1/miller_samantha_madlib/main.py
+++ wk1/miller_samantha_madlib/main.py
@@ -4,8 +4,11 @@ March 4, 2014
 Madlib
 '''
 
+# Asking first friend to input their name
 first_friend = raw_input('Please enter the first friends name: ')
+# Asking second friend to input their name
 second_friend = raw_input('Please enter the second friends name: ')
+# Asking third friend to input their name
 third_friend = raw_input('Please enter the third friends name: ')
 
 first_money = input('Amount of money first friend has: ')
@@ -15,15 +18,28 @@ third_money = input('Amount of money third friend has: ')
 def group_amount(a1, a2, a3):
 	group_total = a1 + a2 + a3
 	return group_total
-group_amount(first_money, second_money, third_money)
+group_amount = group_amount(first_money, second_money, third_money)
 
+def tip(group_total):
+	tip_conversion = group_total / 3 
+	tip_conversion * .20 
+	return tip_conversion
+tip(group_amount)	
 
 menu_dict = dict()
 menu_dict = {'large cheese pizza': 10.50, 'large cheese stuffed crust pizza': 13.50, 'large meat lovers stuffed crust pizza': 15.50}
-print menu_dict
 
+menu_response = []
 
-group_dict = dict()
-group_dict = {first_friend:first_money, second_friend:second_money, third_friend:third_money}
+if group_amount >= menu_dict['large cheese pizza'] and group_amount >= menu_dict['large cheese stuffed crust pizza'] and group_amount >= menu_dict['large meat lovers stuffed crust pizza']:
+	menu_response.append('You have enough money for a large cheese pizza, large cheese stuffed crust pizza or a large meat lovers stuffed crust pizza')
+elif group_amount >= menu_dict['large cheese pizza'] and group_amount >= menu_dict['large cheese stuffed crust pizza']:
+	menu_respons.append('You have enough money for a large cheese pizza or large cheese stuffed crust pizza.')
+elif group_amount >= menu_dict['large cheese pizza']:
+	menu_response.append('You have enough money for a large cheese pizza')
+else:
+	menu_response.append('You do not have enough money for anything at this pizzaria')
 
+				 
+	
 
