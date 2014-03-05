a8b654da997fca98dd2ca492646ee244a98b2af0
Samantha <miller.m.samantha@gmail.com>
2014-03-04 22:17:23 -0500

    Set up string and number input prompts from the user.

---
+++ wk1/miller_samantha_madlib/main.py
@@ -0,0 +1,14 @@
+'''
+Samantha Miller
+March 4, 2014
+Madlib
+'''
+
+first_friend = raw_input('Please enter the first friends name: ')
+second_friend = raw_input('Please enter the second friends name: ')
+third_friend = raw_input('Please enter the third friends name: ')
+
+first_money = input('Amount of money first friend has: ')
+second_money = input('Amount of money second friend has: ')
+third_money = input('Amount of money third friend has: ')
+
