3ed9db65671342fd82fb0ef461e520194fa7711f
Samantha <miller.m.samantha@gmail.com>
2014-03-10 17:18:30 -0400

    Set up body with form attributes, made iterations and set up footer variable

--- wk1/miller-samantha-serverside-form/form.py
+++ wk1/miller-samantha-serverside-form/form.py
@@ -1,24 +1,29 @@
 class Form():
 	def __init__(self, main_self):
 		pass
+		# Creating a head variable to contain the html head code
 		self.head = ''' <!DOCTYPE HTML>
 		<html>
 			<head>
 				<title>Address Book</title>
 			</head> 
 		<body>'''
+		# Creating a form variable to contain the html form code
 		self.form = ''' 
-		<form method='GET' action='' 
-		<input type='text' name='first_name' placeholder='First Name'/>
-		<input type='text' name='last_name' placeholder='Last Name'/>
-		<input type='text' name='phone_number' placeholder='Phone Number'/>
-		<select>
-			<option value="Home">Home</option>
-	  		<option value="Cell">Cell</option>
-	  		<option value="Work">Work</option>
-		</select>
-		<input type='checkbox' name='relationship' value='Friend'/>
-		<input type='checkbox' name='relationship value='Family'/>
-		<input type='checkbox' name='relationship value='Co-worker'/>
+		<form method='GET' action=''> 
+			<input type='text' name='first_name' placeholder='First Name'/>
+			<input type='text' name='last_name' placeholder='Last Name'/>
+			<input type='text' name='phone_number' placeholder='Phone Number'/>
+			<select>
+				<option value='Home'>Home</option>
+		  		<option value='Cell' selected='selected'>Cell</option>
+		  		<option value='Work'>Work</option>
+			</select>
+			<input type='checkbox' name='relationship' value='Friend'/>
+			<input type='checkbox' name='relationship value='Family'/>
+			<input type='checkbox' name='relationship value='Co-worker'/>
+		</form>	
 		'''
+		self.footer = '''</body></html>'''
+
 
