67d003846a6149d24d6d68f6b0edb138f591a039
Samantha <miller.m.samantha@gmail.com>
2014-03-10 17:13:29 -0400

    Set up the html form

--- wk1/miller-samantha-serverside-form/form.py
+++ wk1/miller-samantha-serverside-form/form.py
@@ -7,4 +7,18 @@ class Form():
 				<title>Address Book</title>
 			</head> 
 		<body>'''
+		self.form = ''' 
+		<form method='GET' action='' 
+		<input type='text' name='first_name' placeholder='First Name'/>
+		<input type='text' name='last_name' placeholder='Last Name'/>
+		<input type='text' name='phone_number' placeholder='Phone Number'/>
+		<select>
+			<option value="Home">Home</option>
+	  		<option value="Cell">Cell</option>
+	  		<option value="Work">Work</option>
+		</select>
+		<input type='checkbox' name='relationship' value='Friend'/>
+		<input type='checkbox' name='relationship value='Family'/>
+		<input type='checkbox' name='relationship value='Co-worker'/>
+		'''
 
