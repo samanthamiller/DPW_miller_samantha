4ec93d1830be9722c68df8c109cc3bce6ea8dffb
Samantha <miller.m.samantha@gmail.com>
2014-03-11 13:57:42 -0400

    Commented main.py file

--- wk1/miller-samantha-serverside-form/main.py
+++ wk1/miller-samantha-serverside-form/main.py
@@ -21,31 +21,50 @@
 # limitations under the License.
 #
 import webapp2
+# From the python file form import the class Form
 from form import Form
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		if self.request.GET:
+			# Creating empty sting variable that will later be used to populate form values onto the page.
 			relationship = ''
+
+			# Using try: to prevent checkbox errors
 			try:
+				# Set relationship_type equal to the value of the 'relationship1' checkbox if checked
 				relationship_type = self.request.GET['relationship1']
+				# If the person did check the 'relationship1' checkbox 
 				if relationship_type:
-					relationship = relationship + relationship_type + ' '
+					# Then set empty string variable equal to itself plus the new relationship variable type which will be equal to the checkbox 'relationship1'
+					relationship = relationship + relationship_type + ' '	
+			# If they check neither box no error will happen			
 			except StandardError:
 				pass
 
+			# Using try: to prevent checkbox errors
 			try:
+				# Set relationshiip_type2 equal to the value of the "relationship2" checkbox if checked
 				relationship_type2 = self.request.GET['relationship2']
+				# If the person did check the 'relationship2' checkbox
 				if relationship_type2:
+					# Then set empty string variable equal to itself plus the new relationship variable type which will be equal to the checkbox 'relationship2'
+					# Also because of this both can be checked 
 					relationship = relationship + relationship_type2
+			# If they check neither box no error will happen		
 			except StandardError:
 				pass
 			
+			# Set variable form_info equal to all the user inputed form information
 			form_info = self.request.GET['first_name'] + ' ' + self.request.GET['last_name'] + ' ' + self.request.GET['phone_type'] + ' ' + self.request.GET['phone_number'] + ' ' + relationship
-			form = Form(self) #Creates form Object
+			# Creates form Object
+			form = Form(self) 
+			# Will populate the user inputed information on a new Form Page
 			self.response.write(form.print_contents(form_info))
 		else:
-			form = Form(self) #Creates form Object
+			# Creates form Object
+			form = Form(self)
+			# Will populate the form for the user to input it's information
 			self.response.write(form.print_contents())
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
