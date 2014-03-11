a369414ce29589f7ff85848b46f81f5ca2bf7c46
Samantha <miller.m.samantha@gmail.com>
2014-03-11 18:55:59 -0400

    Things and stuff

---
+++ wk2/getters-setters/main.py
@@ -0,0 +1,41 @@
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+    	# self.attribute - without 'self' it is a variable local to the function
+    	self.t = Transcript()
+    	self.t.grade1 = 90
+    	self.t.grade2 = 94
+    	self.t.quiz1 = 72
+    	self.t.quiz2 = 95
+    	# self.t.final_grade = self.t.calc_grade()
+    	# To access the property function
+    	print self.t.final_grade
+
+
+# The 'object' inhearatince is important
+class Transcript(object):
+	def __init__(self):
+		self.grade1 = 0
+		self.grade2 = 0
+		self.quiz1 = 0
+		self.quiz2 = 0
+		self.__final_grade = 0
+
+		# public
+		# _ protected
+		# __ private
+
+		# Make final_grade a property with the '@property' decorator
+	@property
+	def final_grade(self):
+		return str(self.__final_grade) + ' is your final grade.'
+
+
+	def calc_grade(self):
+		avg = (self.grade1 + self.grade2 + self.quiz1 + self.quiz2)/4
+		return avg
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
