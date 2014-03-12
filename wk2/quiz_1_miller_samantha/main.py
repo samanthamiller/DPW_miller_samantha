4460bf37a2fd5e89634a4edb47e0ad7d4407c661
Samantha <miller.m.samantha@gmail.com>
2014-03-11 21:42:09 -0400

    Wrote out first problem, created class, created init function

--- wk2/quiz_1_miller_samantha/main.py
+++ wk2/quiz_1_miller_samantha/main.py
@@ -1,1 +1,15 @@
-print 'test'
+class Quiz1:
+	def __init__(self):
+		print 
+	def calcArea(h,w):
+		h = raw_input('Enter the height of the object: ')
+		w = raw_input('Enter the width of the object: ')
+		area = h*w
+		return area
+		if h==w:
+			shape = 'This object is a square'
+			return shape
+		else:
+			shape = 'This object is a rectangle'
+			return shape	
+
