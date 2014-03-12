63e9fb49e465a2331c79e91b1bcc082c38d73c1d
Samantha <miller.m.samantha@gmail.com>
2014-03-12 17:03:29 -0400

    Placed calc_total method for each of the instantiations of the class Bill

--- wk2/miller-samantha-grade-avg/main.py
+++ wk2/miller-samantha-grade-avg/main.py
@@ -14,6 +14,8 @@ class MainHandler(webapp2.RequestHandler):
 		self.table_1.plate3 = 6.53
 		self.table_1.plate4 = 8.96
 		self.table_1.plate4 = 6.78
+		self.table_1.calc_total()
+		print self.table_1.total
 
 		self.table_2 = RestaurantBill()
 		self.table_2.plate1 = 8.95
@@ -21,6 +23,8 @@ class MainHandler(webapp2.RequestHandler):
 		self.table_2.plate3 = 3.99
 		self.table_2.plate4 = 12.50
 		self.table_2.plate4 = 14.78
+		self.table_2.calc_total()
+		print self.table_2.total
 
 		self.table_3 = RestaurantBill()
 		self.table_3.plate1 = 3.75
@@ -28,6 +32,8 @@ class MainHandler(webapp2.RequestHandler):
 		self.table_3.plate3 = 6.53
 		self.table_3.plate4 = 19.23
 		self.table_3.plate4 = 5.95
+		self.table_3.calc_total()
+		print self.table_3.total
 
 		self.table_4 = RestaurantBill()
 		self.table_4.plate1 = 2.50
@@ -35,6 +41,8 @@ class MainHandler(webapp2.RequestHandler):
 		self.table_4.plate3 = 10.35
 		self.table_4.plate4 = 7.88
 		self.table_4.plate4 = 4.81
+		self.table_4.calc_total()
+		print self.table_4.total
 
 		self.table_5 = RestaurantBill()
 		self.table_5.plate1 = 13.40
@@ -42,6 +50,8 @@ class MainHandler(webapp2.RequestHandler):
 		self.table_5.plate3 = 8.74
 		self.table_5.plate4 = 9.99
 		self.table_5.plate4 = 9.87
+		self.table_5.calc_total()
+		print self.table_5.total
 
 class RestaurantBill(object):
 	def __init__(self):
@@ -60,7 +70,7 @@ class RestaurantBill(object):
 	def total(self, t):
 		self.__total = t
 
-	def calc_total(self)
+	def calc_total(self):
 		total = self.plate1 + self.plate2 + self.plate3 + self.plate4 + self.plate5
 		self.__total = total
 
