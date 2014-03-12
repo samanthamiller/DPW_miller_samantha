d7fe49662bbe2f0ceda51a480d03329717282407
Samantha <miller.m.samantha@gmail.com>
2014-03-11 22:12:27 -0400

    Completed quiz

--- wk2/quiz_1_miller_samantha/main.py
+++ wk2/quiz_1_miller_samantha/main.py
@@ -13,5 +13,5 @@ width = raw_input('Enter the width of the object: ')
 area = calcArea(int(height),int(width))	
 print 'The area of your  is ' +str(area)+ ' square feet.'
 
-for i in range(1,100,1):
-	print str(i)+ 'Bottles of Beer on the Wall, ' +str(i)+ 'Bottles of Beer.. take one down and pass it around. Now you have' +str(i)+ 'bottles of beer on the wall!'
+for i in range(100,1, -1):
+	print str(i)+ ' Bottles of Beer on the Wall, ' +str(i)+ ' Bottles of Beer.. take one down and pass it around. Now you have ' +str(i)+ ' bottles of beer on the wall!'
