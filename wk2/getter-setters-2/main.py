4d84f8298d8e7236dd98f6b0f55a0e2bf2d86efc
Samantha <miller.m.samantha@gmail.com>
2014-03-11 20:07:53 -0400

    Going over links

--- wk2/getter-setters-2/main.py
+++ wk2/getter-setters-2/main.py
@@ -1,9 +1,18 @@
 import webapp2
+from page import Page
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+	def get(self):
+    	# Triggers __init__ function in Page() class
+		self.page = Page()
+		self.page.title = 'Contact Us'
+		self.response.write(self.page.all)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+# Data Objects
+# Classes with no function
+# No methods in it 
+
---
+++ wk2/getter-setters-2/page.py
@@ -0,0 +1,38 @@
+class Page(object):
+	def __init__(self):
+		self.head = ''' <!DOCTYPE HTML>
+		<html>
+			<head>
+				<title>{self.title}</title>
+			</head>
+			<body>		
+		'''
+		self.body = ''' 
+		<a href=''>Strawberry</a>
+		<a href=''>Pear</a>
+		<a href=''>Banana</a>
+		'''
+		self.close = '''
+		</body>
+		</html>
+		'''
+		# self.all = self.head + self.body + self.close
+		self.update_page()
+		self.__title = 'Welcome!'
+	def update_page(self):
+		self.all = self.head + self.body + self.close
+	
+	# Property for __title 	
+	@property
+	def title(self):
+		return self.__title
+
+	@title.setter
+	def title(self, title):
+		self.__title = title
+		# Reformat the self.all string so that it incorporates our title
+		self.all = self.all.format(**locals())
+
+
+
+		
