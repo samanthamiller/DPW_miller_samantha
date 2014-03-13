dc4028c746db445b9e8a729c3ef8b87d24011990
Samantha <miller.m.samantha@gmail.com>
2014-03-12 22:12:10 -0400

    Added links

--- wk2/miller-samantha-grade-avg/detail.py
+++ wk2/miller-samantha-grade-avg/detail.py
@@ -7,4 +7,12 @@ class Detail(object):
 			<head>
 			<body> 
 		'''
-		
+
+		self.__form = ''' 
+			<form action="" method="GET" name="bills" id="links">
+				<a href="/?bill=1" name="bill" id="table1">Tabel 1</a>
+				<a href="/?bill=2" name="bill" id="table2">Tabel 2</a>
+				<a href="/?bill=3" name="bill" id="table3">Tabel 3</a>
+				<a href="/?bill=4" name="bill" id="table4">Tabel 4</a>
+				<a href="/?bill=5" name="bill" id="table5">Tabel 5</a>
+		'''
