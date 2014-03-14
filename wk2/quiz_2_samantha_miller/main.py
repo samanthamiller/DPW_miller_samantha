71a3eacdf4590db270b14b3b397d61148ad15515
Samantha <miller.m.samantha@gmail.com>
2014-03-13 22:01:31 -0400

    Finished first class

--- wk2/quiz_2_samantha_miller/main.py
+++ wk2/quiz_2_samantha_miller/main.py
@@ -1,3 +1,25 @@
 class Cats(object):
 	def __init__(self):
-		pass
+		self.__name = ''
+		self.__breed = ''
+		self.age = 0
+
+		@property
+		def age(self):
+			return self.__age
+
+		@age.setter
+		def age(self, years):
+			self.__age = years
+
+	def print_info(n, b, a):
+		cat_info = n + ' is a ' + a + ' year old ' + b + ' cat.'
+		print cat_info
+
+	def print_cat_name(a):
+		name = a
+		print name
+
+class VetBill(object):
+	def __init__(self):
+		
