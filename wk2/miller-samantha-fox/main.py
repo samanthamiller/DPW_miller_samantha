48c2246ee2eede9ad22ecc89e8e1197b32a290a9
Samantha <miller.m.samantha@gmail.com>
2014-03-13 23:35:50 -0400

    Added all of the attributes to the animal class

--- wk2/miller-samantha-fox/main.py
+++ wk2/miller-samantha-fox/main.py
@@ -8,6 +8,17 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write(details.body)
         self.response.write(details.footer)
 
+class Animal(object):
+	def __init__(self):
+		self.phylum = ''
+		self.animal_class = ''
+		self.order = ''
+		self.family = ''
+		self.genus = ''
+		self.image = ''
+		self.lifespan = ''
+		self.habitat = ''
+		self.geolocation = ''
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
