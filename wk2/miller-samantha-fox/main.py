045d9e43fd90779d2d1e239f2c73cb9db693897d
Samantha <miller.m.samantha@gmail.com>
2014-03-13 23:48:02 -0400

    Added in lion class as a subclass of the animal class

--- wk2/miller-samantha-fox/main.py
+++ wk2/miller-samantha-fox/main.py
@@ -19,6 +19,20 @@ class Animal(object):
 		self.lifespan = ''
 		self.habitat = ''
 		self.geolocation = ''
+
+class Lion(Animal):
+	def __init__(self):
+		super(Lion,self).__init__()
+		self.phylum = 'Chordata'
+		self.animal_class = 'Mammalia'
+		self.order = 'Carnivora'
+		self.family = 'Felidea'
+		self.genus = 'Panthera'
+		self.image = ''
+		self.lifespan = '15'
+		self.habitat = 'Sub-Saharan Africa except in desert and rainforest habitats.'
+		self.geolocation = ''
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
