81ab770b433753b9ac986f35aaa3d989dd941536
Samantha <miller.m.samantha@gmail.com>
2014-03-20 21:38:29 -0400

    Instantiated and printed first subclass information

--- wk3/miller-samantha-quiz3/main.py
+++ wk3/miller-samantha-quiz3/main.py
@@ -2,7 +2,16 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
-		pass
+		# Instantiate shoes class
+		shoes = Shoes()
+
+		heels = Heels()
+		heels.brand = 'Derion '
+		heels.color = 'Brown '
+		heels.size = '7 '
+		heels.heel_size = '6 '
+		heels.type = 'Boot '
+		heels.printInfo()
 
 class Shoes(object):
 	def __init__(self):
@@ -14,6 +23,10 @@ class Shoes(object):
 	def shoeBrand(self):
 		return self._size
 
+	@property
+	def shoeColor(self):
+		return self.color
+
 	def printSize(self):
 		print self.size
 
@@ -23,12 +36,15 @@ class Shoes(object):
 
 class Heels(Shoes):
 	def __init__(self):
-		super(Wedges,self).__init__()
+		super(Heels,self).__init__()
 		self.heel_size = ''
 		self.type = ''
 	@property
 	def heel_height(self):
 		return self.heel_size
+
+	def printInfo(self):
+		print self.brand + self.color + self.heel_size + self.type
 
 class Sneakers(Shoes):
 	def __init__(self):
@@ -39,6 +55,9 @@ class Sneakers(Shoes):
 	@property
 	def sport(self):
 		return self.athletic_type
+
+	def printInfo(self):
+		print self.brand + self.color + self.athletic_type + self.grip_level
 
 
 app = webapp2.WSGIApplication([
