853589b5e1af4f71db478059d3a87717b512384c
Samantha <miller.m.samantha@gmail.com>
2014-03-20 21:11:37 -0400

    Deleted unneccesary content

--- wk3/yahoo-weather-mvc/main.py
+++ wk3/yahoo-weather-mvc/main.py
@@ -5,6 +5,7 @@ import urllib2
 from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
+	''' This is the main controler for my weather application '''
 	def get(self):
 		page = FormPage()
 		page.inputs = {'zip':'text', 'Submit':'submit'}
@@ -17,11 +18,16 @@ class MainHandler(webapp2.RequestHandler):
 			zip = self.request.GET['zip']
 
 			wm = WeatherModel(zip) # Sends zip to model
-			wm.send()
+			wm.send() # Tells model to send the info and get the data
+			wv = WeatherView() # Creates view
+			print 'View got data from model'
+			wv.do = wm.do # Pass data from model to view
+			self.response.write(wv.content)
 
 
 
 class WeatherModel(object):
+	'''  This model handles fetching, parsing and sorting data from the weather api '''
 	def __init__(self, zip):
 		self.__url = 'http://xml.weather.yahoo.com/forecastrss?p='
 		self.__request = urllib2.Request(self.__url + zip)
@@ -32,21 +38,26 @@ class WeatherModel(object):
 		self.sort() # Call the method that sorts the xml doc
 
 	def sort(self):
-		print 'Sort function ran'
 		self.__xmldoc = minidom.parse(self.__result)
-		self.__do = WeatherData()
-		self.__do.title = self.__xmldoc.getElementsByTagName('title')[2].firstChild.nodeValue
+		self.__dos = []
+
 		list = self.__xmldoc.getElementsByTagName('yweather:forecast')
 		for l in list:
-			print l.attributes['day'].value
-			self.__do.code = l.attributes['code'].value
-			self.__do.day = l.attributes['day'].value
-			self.__do.high = l.attributes['high'].value
-			self.__do.low = l.attributes['low'].value
-			self.__do.condition = l.attributes['text'].value
+			do = WeatherData()
+			do.code = l.attributes['code'].value
+			do.day = l.attributes['day'].value
+			do.high = l.attributes['high'].value
+			do.low = l.attributes['low'].value
+			do.condition = l.attributes['text'].value
+			self.__dos.append(do)
+
+	@property
+	def dos(self):
+		return self.__dos # Returns data object with all our delicious info
 
 
 class WeatherData(object):
+	'''  This data object holds the dta fetched by the model and shown by the view '''
 	def __init__(self):
 		# Data objects are public
 		self.title = ''
@@ -56,7 +67,38 @@ class WeatherData(object):
 		self.code = ''
 		self.condition = ''
 
+class WeatherView(object):
+	''' This class handles how the data is shown to the user '''
+	def __init__(self):
+		self.__do = WeatherData()
+		print 'Weather View function ran'
+
+	def update(self): # Update the apperance of the view
+		self.__content = '<h3>' + self.__do.title + '</h3>'
+		self.__content += self.__do.day + ' -'
+		self.__content += ' High: ' + self.__do.high
+		self.__content += ' Low: ' + self.__do.low
+		self.__content += ' Condition: ' + self.__do.condition
+		self.__content += '<br/>'
+		print self.__content
+
+
+	@property
+	def do(self):
+		return self.__do 
+
+	@do.setter
+	def do(self,new_do):
+		self.__do = new_do
+		self.update()
+		print self.__do.title
+
+	@property
+	def content(self):
+		return self.__content
+
 class Page(object):
+	'''  This class handles basic html components of the web page '''
 	def __init__(self):
 		self._head = '''  <!DOCTYPE HTML>
 		<html>
@@ -82,6 +124,7 @@ class Page(object):
 		return self._head + self._body + self._footer
 
 class FormPage(Page):
+	'''  This handles form components and html components for web page '''
 	def __init__(self):
 	# Run the instantiating function for the Super Class
 	# Pass in the name of the subclass and self
