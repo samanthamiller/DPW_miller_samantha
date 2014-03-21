4042eab356bb47c517e09aa9d5faf1f70ea8d893
Samantha <miller.m.samantha@gmail.com>
2014-03-20 21:13:58 -0400

    Set up basic super class and init function

--- wk3/miller-samantha-quiz3/main.py
+++ wk3/miller-samantha-quiz3/main.py
@@ -2,7 +2,10 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        
+
+class Shoes(object):
+	def __init__(self):
+		pass
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
